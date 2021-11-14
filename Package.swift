390b3859b70c65b1c20edaf41727162321db9b4f
mattorb <matt@piikeastreet.com>
2021-11-14 12:09:18 -0600

    Exclude Test sources from main target

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LogWatcher",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["Tests"]),
         .testTarget(
             name: "LogWatcherTests",
             dependencies: ["LogWatcher"])
